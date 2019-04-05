9e42387a42af16a4df9a9d52f6f90680aa3d6e1f
Thomas FP <tom@footprintinvestigations.com>
2019-04-05 17:04:04 +0100

    Modules 1 2
    Follow along pluralsight course modules 1, 2 and most of module 3
    Up to "Getting an individual item" in "Your first API"

---
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TheCodeCamp.Data;
+using TheCodeCamp.Models;
+
+namespace TheCodeCamp.Controllers
+{
+	[RoutePrefix("api/camps")]
+    public class CampsController : ApiController
+    {
+        private IMapper _mapper;
+        private ICampRepository _repository;
+        public CampsController(ICampRepository repository,
+                                IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+		[Route()]
+        public async Task<IHttpActionResult> Get()
+        {
+            try
+            {
+                var result = await _repository.GetAllCampsAsync();
+
+                // Mapping
+                var mappedResult = _mapper.Map<IEnumerable<CampModel>>(result);
+
+                return Ok(mappedResult);
+            }
+            catch (System.Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+				if (result == null ) { return NotFound(); }
+                var mappedResult = _mapper.Map<CampModel>(result);
+                return Ok(mappedResult);
+            }
+            catch (System.Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+
+
+        /*
+                public async Task<IHttpActionResult> Action()
+                {
+                    try
+                    {
+
+                    }
+                    catch (System.Exception e)
+                    {
+                        return InternalServerError(e);
+                    }
+                }
+
+        */
+    }
+}
+
